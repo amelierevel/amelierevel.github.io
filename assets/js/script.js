2b81b68895f91c0cca67c363b8e3a5ae00e6d889
Amélie Revel <amelie.revel20@yahoo.fr>
2018-07-13 12:25:02 +0200

    petites modif cv

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//Animation expériences
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.sucrerie', {duration:3000});
+sr.reveal('.smdo', {duration:3000});
+sr.reveal('.mogalakwena',{duration:3000});
+sr.reveal('.pad', {duration:3000});
